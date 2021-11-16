a91627bdc2c1506d30ac05e2aa9942d96a2655ee
Armedi <armedi@outlook.com>
2021-11-16 07:33:24 +0700

    basic plate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { createPlateComponents, Plate } from '@udecode/plate'
 
-import { Plate } from '@udecode/plate'
+import { CONFIG } from './config/config'
+import { plugins } from './config/plugins'
 
+const components = createPlateComponents()
 
-const App = () => <Plate />
+const App = () => (
+  <Plate
+  plugins={plugins}
+  components={components}
+  options={CONFIG.options}
+  editableProps={CONFIG.editableProps}
+  />
+)
 
 export default App
